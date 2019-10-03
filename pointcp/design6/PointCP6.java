9dbe6932d546d785c5aa592dc17da4970e89e5f8
Olivia Borel <obore031@uottawa.ca>
2019-10-02 23:48:27 -0400

    last edits

--- pointcp/design6/PointCP6.java
+++ pointcp/design6/PointCP6.java
@@ -4,10 +4,7 @@ public interface PointCP6{
   double getY();
   double getRho();
   double getTheta();
-  //public PointCP2 convertStorageToPolar();
-  //public PointCP3 convertStorageToCartesian();
   double getDistance(PointCP pointB);
   PointCP2 rotatePoint(double rotation);
-  //public String toString();
 
 }
